b6d8ef894840dd70c0ae368806ba7d1f0c17fa80
Lilian Makuru <147387136+LilianMakuru@users.noreply.github.com>
2024-08-31 08:26:16 +0300

    Add files via upload

---
+++ user_input_082130.py
@@ -0,0 +1,7 @@
+#Asking for user's name, age, and location
+name = input ("please  enter your name: ")
+age = input ("please  enter your age: ")
+location = input ("please  enter your location: ")
+
+#printing out a personalized message
+print (f" Hello {name}, you are {age} years old and live in {location}.")
